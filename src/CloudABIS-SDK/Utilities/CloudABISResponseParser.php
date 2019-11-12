ebcc0fe2eca130f3e4c83df07c3f9bef1f425748
Ashique <ashique12009@gmail.com>
2019-11-12 13:54:42 +0600

    CloudABISResponseParser class updated

--- src/CloudABIS-SDK/Utilities/CloudABISResponseParser.php
+++ src/CloudABIS-SDK/Utilities/CloudABISResponseParser.php
@@ -8,63 +8,62 @@ class CloudABISResponseParser{
         $detailsMessage = "";
         switch ($operationResult)
         {
-            case CloudABISConstant.CS:
-                $detailsMessage = CloudABISConstant.CS_MESSAGE;
+            case CloudABISConstant::CS:
+                $detailsMessage = CloudABISConstant::CS_MESSAGE;
                 break;
-            case CloudABISConstant.CF:
+            case CloudABISConstant::CF:
                 $detailsMessage = CloudABISConstant.CF_MESSAGE;
                 break;
-            case CloudABISConstant.VS:
-                $detailsMessage = CloudABISConstant.VS_MESSAGE;
+            case CloudABISConstant::VS:
+                $detailsMessage = CloudABISConstant::VS_MESSAGE;
                 break;
-            case CloudABISConstant.VF:
-                $detailsMessage = CloudABISConstant.VF_MESSAGE;
+            case CloudABISConstant::VF:
+                $detailsMessage = CloudABISConstant::VF_MESSAGE;
                 break;
-            case CloudABISConstant.DS:
-                $detailsMessage = CloudABISConstant.DS_MESSAGE;
+            case CloudABISConstant::DS:
+                $detailsMessage = CloudABISConstant::DS_MESSAGE;
                 break;
-            case CloudABISConstant.DF:
-                $detailsMessage = CloudABISConstant.DF_MESSAGE;
+            case CloudABISConstant::DF:
+                $detailsMessage = CloudABISConstant::DF_MESSAGE;
                 break;
-            case CloudABISConstant.MATCH_FOUND:
-                $detailsMessage = CloudABISConstant.MATCH_FOUND_MESSAGE;
+            case CloudABISConstant::MATCH_FOUND:
+                $detailsMessage = CloudABISConstant::MATCH_FOUND_MESSAGE;
                 break;
-            case CloudABISConstant.NO_MATCH_FOUND:
-                $detailsMessage = CloudABISConstant.NO_MATCH_FOUND_MESSAGE;
-                break;
-
-            case CloudABISConstant.YES:
-                $detailsMessage = CloudABISConstant.YES_MESSAGE;
-                break;
-            case CloudABISConstant.NO:
-                $detailsMessage = CloudABISConstant.NO_MESSAGE;
-                break;
-            case CloudABISConstant.CUSTOMER_INFO_NOT_FOUND:
-                $detailsMessage = CloudABISConstant.CUSTOMER_INFO_NOT_FOUND_MESSAGE;
-                break;
-            case CloudABISConstant.ID_NOT_EXIST:
-                $detailsMessage = CloudABISConstant.ID_NOT_EXIST_MESSAGE;
+            case CloudABISConstant::NO_MATCH_FOUND:
+                $detailsMessage = CloudABISConstant::NO_MATCH_FOUND_MESSAGE;
                 break;
 
+            case CloudABISConstant::YES:
+                $detailsMessage = CloudABISConstant::YES_MESSAGE;
+                break;
+            case CloudABISConstant::NO:
+                $detailsMessage = CloudABISConstant::NO_MESSAGE;
+                break;
+            case CloudABISConstant::CUSTOMER_INFO_NOT_FOUND:
+                $detailsMessage = CloudABISConstant::CUSTOMER_INFO_NOT_FOUND_MESSAGE;
+                break;
+            case CloudABISConstant::ID_NOT_EXIST:
+                $detailsMessage = CloudABISConstant::ID_NOT_EXIST_MESSAGE;
+                break;
 
-            case CloudABISConstant.INVALID_ENGINE:
-                $detailsMessage = CloudABISConstant.INVALID_ENGINE_MESSAGE;
+            case CloudABISConstant::INVALID_ENGINE:
+                $detailsMessage = CloudABISConstant::INVALID_ENGINE_MESSAGE;
                 break;
-            case CloudABISConstant.INVALID_REQUEST:
-                $detailsMessage = CloudABISConstant.INVALID_REQUEST_MESSAGE;
+            case CloudABISConstant::INVALID_REQUEST:
+                $detailsMessage = CloudABISConstant::INVALID_REQUEST_MESSAGE;
                 break;
 
-            case CloudABISConstant.INVALID_ICS_TEMPLATE:
-                $detailsMessage = CloudABISConstant.INVALID_ICS_TEMPLATE_MESSAGE;
+            case CloudABISConstant::INVALID_ICS_TEMPLATE:
+                $detailsMessage = CloudABISConstant::INVALID_ICS_TEMPLATE_MESSAGE;
                 break;
-            case CloudABISConstant.INVALID_ISO_TEMPLATE:
-                $detailsMessage = CloudABISConstant.INVALID_ISO_TEMPLATE_MESSAGE;
+            case CloudABISConstant::INVALID_ISO_TEMPLATE:
+                $detailsMessage = CloudABISConstant::INVALID_ISO_TEMPLATE_MESSAGE;
                 break;
-            case CloudABISConstant.INVALID_ANSI_TEMPLATE:
-                $detailsMessage = CloudABISConstant.INVALID_ANSI_TEMPLATE_MESSAGE;
+            case CloudABISConstant::INVALID_ANSI_TEMPLATE:
+                $detailsMessage = CloudABISConstant::INVALID_ANSI_TEMPLATE_MESSAGE;
                 break;
-            case CloudABISConstant.CACHE_NOT_AVAILABLE:
-                $detailsMessage = CloudABISConstant.CACHE_NOT_AVAILABLE_MESSAGE;
+            case CloudABISConstant::CACHE_NOT_AVAILABLE:
+                $detailsMessage = CloudABISConstant::CACHE_NOT_AVAILABLE_MESSAGE;
                 break;
 
             default:
